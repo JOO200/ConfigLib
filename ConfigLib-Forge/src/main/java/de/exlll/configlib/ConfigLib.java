bac84a0f6e1d62a09489ca9b4f85ef58290c57c3
Johannes Zangl <git@johannes.fam-zangl.net>
2017-09-13 16:37:33 +0200

    Forge implementation (Forge version 1.12.1-14.22.0.2475)

---
+++ ConfigLib-Forge/src/main/java/de/exlll/configlib/ConfigLib.java
@@ -0,0 +1,10 @@
+package de.exlll.configlib;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ConfigLib.MODID, version = ConfigLib.VERSION)
+public class ConfigLib {
+    public static final String MODID = "ConfigLib";
+    public static final String VERSION = "1.4.0";
+}
